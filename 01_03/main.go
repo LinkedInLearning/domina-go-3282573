460573b941a7d972cb91d977b7799251542d88f0
Lincy Gonzalez <lincgr@gmail.com>
2023-09-27 22:23:44 +0200

    01_03 Conversión de tipos de datos en Go

---
+++ 01_03/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+	var numeroEntero int
+	var numeroFlotante float64
+	var numeroString string
+
+	// Convertir de int a float
+	numeroEntero = 12
+
+	// Convertir de float a int
+	numeroFlotante = 32.9
+
+	// Convertir de int a string
+	numeroEntero = 39
+
+	// Convertir de string a int
+	numeroString = "50"
+}
