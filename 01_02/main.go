88e816c728aeffcd4a8ad1d87cf47f393c1b64fe
Lincy Gonzalez <lincgr@gmail.com>
2023-09-27 21:46:46 +0200

    01_02 Crear cadenas multilínea en Go

---
+++ 01_02/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var mensaje string
+	mensaje = "Acumulá Casback por tus compras en la tienda Patitos feliz. Al realizar compras con tu tarjeta de cliente frecuente en la tienda Patitos feliz por un monto mínimo de 5.000, recibí:	Hasta 10% de Cashback acumulable y canjeable únicamente en la aplicación de la tienda Patitos feliz."
+	fmt.Println(mensaje)
+}
