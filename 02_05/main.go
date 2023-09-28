fe95aa1f5d6c5338e5407a9b4eb912563ff42565
Lincy Gonzalez <lincgr@gmail.com>
2023-09-28 16:22:11 +0200

    02_05 Eliminar clave en el map en Go

---
+++ 02_05/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	clientes := map[string]string{
+		"Karla":     "Naranjo",
+		"Luis":      "Alajuela",
+		"Alejandra": "Heredia",
+	}
+
+	fmt.Println(clientes)
+
+	delete(clientes, "Karla")
+
+	fmt.Println(clientes)
+}
