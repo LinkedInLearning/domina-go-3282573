ba628aa28456e959620352842325f78a96b74129
Lincy Gonzalez <lincgr@gmail.com>
2023-09-29 14:04:18 +0200

    03_05 Retornar un archivo xls en un método GET en Go

---
+++ 03_05/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/descarga", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+
+		} else {
+			http.Error(w, "Recurso no encontrado.", http.StatusNotFound)
+			return
+		}
+	})
+
+	fmt.Println("La aplicación se está ejecutando en 127.0.0.1:8080/descarga")
+	http.ListenAndServe(":8080", nil)
+}
