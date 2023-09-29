56c9b1ad315f1a633f3db8df95544898b11cb417
Lincy Gonzalez <lincgr@gmail.com>
2023-09-29 18:35:08 +0200

    02_02 Concatenar dos slices en Go

---
+++ 02_02/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	primeraQuincena := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	segundaQuincena := []int{16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28}
+}
